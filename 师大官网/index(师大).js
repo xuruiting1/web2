420db117c30fd7df9ee13daae7e21c58af372f67
徐瑞婷 <1263183452@qq.com>
2017-12-03 21:05:48 +0800

    河北师范大学官网

---
+++ 师大官网/index(师大).js
@@ -0,0 +1,56 @@
+
+
+var Nodes = document.getElementsByClassName("impor");
+
+
+			for (var i=0; i<Nodes.length; i++)
+			{
+				
+				Nodes[i].onmouseover = function(){
+					this.className = "impor2";
+				}
+				Nodes[i].onmouseout = function(){
+					this.className = "impor";
+				}
+			}
+			
+
+
+
+// 照片轮换
+var box =document.getElementsByClassName('wrap')[0];
+			//box是数组形式的要注意
+			var i=0;
+			var links=box.children;
+			var timer;
+			function imgtable()
+			{
+				links[i%8].style.display='none';
+				i++;
+				links[i%8].style.display='block';
+			}
+			timer=setInterval(imgtable,2000);
+			box.onmouseover=function(){
+				clearInterval(timer);
+			}
+			box.onmouseout=function(){
+				timer=setInterval(imgtable,1500)//imgtable在 周期执行的函数里面调用
+			}
+			var prev=document.getElementById('prev');
+			var next=document.getElementById('next');
+			prev.onclick=function(){
+				links[i%8].style.display='none';
+				i--;
+				if(i<0)
+				{
+					i=7;
+				}
+				links[i%8].style.display='block';
+	
+			}
+			next.onclick=function(){
+				imgtable();
+			}
+
+
+			
